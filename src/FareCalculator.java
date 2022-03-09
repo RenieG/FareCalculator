0b6c5db55e00cff9c1a5658cdb230066243b0a0f
RenieG <igakaho@yahoo.com>
2022-03-08 22:27:52 -0600

    Initial commit Fare Calculator based on age

---
+++ src/FareCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class FareCalculator {
+    public static void main(String []arg) {
+
+        int age;
+        double tax, standardPrice, totalPrice, totalTax;
+
+        totalPrice = 0;
+        tax = 0.13;
+        System.out.println("Please enter your age");
+        Scanner userinput = new Scanner(System.in);
+        age = userinput.nextInt();
+    }
+
+}
